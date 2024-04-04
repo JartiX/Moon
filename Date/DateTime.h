1bf8ec9533b51defede59bcfba18eaa7dc51890f
JartiX <artempilavin@gmail.com>
2024-04-04 18:50:57 +0800

    UPD

---
+++ Date/DateTime.h
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+class Datetime {
+public:
+	int day, month, year, hour, minute, second;
+	double julian_date;
+	Datetime();
+	Datetime(int _day, int _month, int _year);
+	Datetime(int _day, int _month, int _year, int _hour, int _minute, int _second);
+	Datetime(string date);
+
+	friend ostream& operator<<(ostream& out, const Datetime& date);
+	friend istream& operator>>(istream& in, Datetime& date);
+
+	// Вычислить разницу между датами в днях, сложить две даты
+	int& operator -(const Datetime& date) const;
+	int& operator +(const Datetime& date) const;
+
+	// Добавить / вычесть дни из даты
+	Datetime& operator -(const int& _day) const;
+	Datetime& operator +(const int& _day) const;
+
+	// Сравнить даты
+	bool operator <(const Datetime& date) const;
+	bool operator >(const Datetime& date) const;
+	bool operator <=(const Datetime& date) const;
+	bool operator >=(const Datetime& date) const;
+	bool operator ==(const Datetime& date) const;
+	bool operator !=(const Datetime& date) const;
+
+	// Вычислить день недели для заданной даты
+	int Calculate_day_of_week() const;
+
+	// Вычислить день в году для заданной даты
+	int Calculate_day_of_year() const;
+
+	// Проверка года на високосность
+	bool check_leap_year() const;
+
+	// Вычисление юлианской даты
+	double Julian_days() const;
+
+	// Проверка даты на валидность
+	bool isDateValid();
+};
+
+// Вычисление календарной даты по юлианской
+Datetime convert_date(double julian);
---
+++ Moon.cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include "Date/DateTime.h"
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+struct row {
+	Datetime data;
+	double El;
+};
+
+void make_hms(Datetime& _date, char _HMS[7]) {
+	_date.hour = (_HMS[0] - 48) * 10 + (_HMS[1] - 48);
+	_date.minute = (_HMS[2] - 48) * 10 + (_HMS[3] - 48);
+	_date.second = (_HMS[4] - 48) * 10 + (_HMS[5] - 48);
+}
+
+row make_row(istringstream& isstr, string current_date) {
+	char HMS[7];
+	row result;
+	result.data = Datetime(current_date);
+	isstr >> HMS >> result.El >> result.El >> result.El;
+
+	make_hms(result.data, HMS);
+	return result;
+
+}
+
+void calculate(string path, Datetime full_date) {
+	ifstream file(path);
+	if (!file.is_open()) {
+		cerr << "Ошибка при чтении файла";
+	}
+
+	char line[100];
+
+	string curr_date;
+	row my_row;
+	vector<row> vect;
+	size_t ptr = 1;
+
+	bool is_found_date = false;
+	double max_El = -9999;
+	Datetime date;
+
+	file.getline(line, 100);
+	file.getline(line, 100);
+
+	cout << "Дата: " << full_date << endl;
+
+	istringstream iss(line);
+	iss >> curr_date;
+	my_row = make_row(iss, curr_date);
+	vect.push_back(my_row);
+
+	while (true) {
+		file.getline(line, 100);
+		istringstream iss(line);
+		iss >> curr_date;
+		if (!is_found_date and (stoi(curr_date.substr(4, 2)) != full_date.month or stoi(curr_date.substr(6, 2)) != full_date.day)) {
+			continue;
+		}
+		my_row = make_row(iss, curr_date);
+		is_found_date = true;
+
+		vect.push_back(my_row);
+
+		if (vect[ptr - 1].El < 0 and vect[ptr].El >= 0) {
+			cout << "Восход луны:" << vect[ptr].data << endl;
+		}
+		else if (vect[ptr - 1].El > 0 and vect[ptr].El <= 0) {
+			cout << "Заход луны:" << vect[ptr].data << endl;
+		}
+		if (max_El < vect[ptr].El) {
+			date = vect[ptr].data;
+			max_El = vect[ptr].El;
+		}
+		ptr++;
+
+		if (is_found_date and (stoi(curr_date.substr(6, 2)) != full_date.day or stoi(curr_date.substr(4, 2)) != full_date.month)) {
+			break;
+		}
+	}
+	cout << "Кульминация Луны: " << date;
+	file.close();
+
+}
+int main() {
+	setlocale(LC_ALL, "ru-RU");
+	string path = "Moon/moon";
+
+	Datetime full_date(6, 3, 2023);
+	//try {
+	//	cin >> full_date; // day-month-year
+	//}
+	//catch (const string& ex) {
+	//	cerr << ex;
+	//}
+
+	path += to_string(full_date.year) + ".dat";
+
+	calculate(path, full_date);
+}
