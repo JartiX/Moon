37a8116386cf1b1974273ac723897a6c42b15602
JartiX <artempilavin@gmail.com>
2024-04-05 00:03:39 +0800

    Update Moon.cpp

--- Moon.cpp
+++ Moon.cpp
@@ -5,24 +5,29 @@
 #include <string>
 using namespace std;
 
-struct row {
-	Datetime data;
-	double El;
-};
 
-void make_hms(Datetime& _date, char(&_HMS)[7]) {
-	_date.hour = (_HMS[0] - 48) * 10 + (_HMS[1] - 48);
-	_date.minute = (_HMS[2] - 48) * 10 + (_HMS[3] - 48);
-	_date.second = (_HMS[4] - 48) * 10 + (_HMS[5] - 48);
-}
 
-void make_row(istringstream& isstr, string& current_date, row& result) {
-	char HMS[7];
-	result.data = Datetime(current_date);
-	isstr >> HMS >> result.El >> result.El >> result.El;
+//void make_date(Datetime& _date, char(&_HMS)[7], char(&current_date)[9]) {
+//	try {
+//		_date.hour = (_HMS[0] - 48) * 10 + (_HMS[1] - 48);
+//		_date.minute = (_HMS[2] - 48) * 10 + (_HMS[3] - 48);
+//		_date.second = (_HMS[4] - 48) * 10 + (_HMS[5] - 48);
+//		_date.year = 1000 * (current_date[0] - 48) + 100 * (current_date[1] - 48) + 10 * (current_date[2] - 48) + (current_date[3] - 48);
+//		_date.day = 10 * (current_date[6] - 48) + (current_date[7] - 48);
+//		_date.month = 10 * (current_date[4] - 48) + (current_date[5] - 48);
+//	}
+//	catch (...) {
+//		return;
+//	}
+//}
 
-	make_hms(result.data, HMS);
-}
+//void make_row(istringstream& isstr, char(&current_date)[9], row& result) {
+//	char HMS[7];
+//	result.data = Datetime();
+//	isstr >> HMS >> result.El >> result.El >> result.El;
+//
+//	make_date(result.data, HMS, current_date);
+//}
 
 void calculate(string path, Datetime full_date) {
 	ifstream file(path);
@@ -33,14 +38,16 @@ void calculate(string path, Datetime full_date) {
 	char line[100];
 
 	char curr_date[9];
-	string curre_date;
+	char HMS[7];
 
-	row last_row;
-	row my_row;
+	double last_El;
+	double El;
 
 
 	bool is_found_date = false;
 	double max_El = -9999;
+	Datetime answer_date;
+	answer_date = Datetime();
 	Datetime date;
 
 	file.getline(line, 100);
@@ -49,42 +56,51 @@ void calculate(string path, Datetime full_date) {
 	cout << "Дата: " << full_date << endl;
 
 	istringstream iss(line);
-	iss >> curr_date;
-	curre_date = curr_date;
-	make_row(iss, curre_date, last_row);
+	iss >> HMS >> HMS >> last_El >> last_El >> last_El;
 
 	while (true) {
 		file.getline(line, 100);
 		istringstream iss(line);
 		iss >> curr_date;
-		curre_date = curr_date;
 		if (!is_found_date and ((10 * (curr_date[4] - 48) + (curr_date[5] - 48)) != full_date.month or
 			(10 * (curr_date[6] - 48) + (curr_date[7] - 48)) != full_date.day)) {
 			continue;
 		}
-		try {
-			make_row(iss, curre_date, my_row);
-		}
-		catch (...) {
-			break;
-		}
+		iss >> HMS >> El >> El >> El;
+
 		is_found_date = true;
 
 
-		if (last_row.El < 0 and my_row.El >= 0) {
-			cout << "Восход луны:" << my_row.data << endl;
+		if (last_El < 0 and El >= 0) {
+			answer_date.hour = (HMS[0] - 48) * 10 + (HMS[1] - 48);
+			answer_date.minute = (HMS[2] - 48) * 10 + (HMS[3] - 48);
+			answer_date.second = (HMS[4] - 48) * 10 + (HMS[5] - 48);
+			answer_date.year = 1000 * (curr_date[0] - 48) + 100 * (curr_date[1] - 48) + 10 * (curr_date[2] - 48) + (curr_date[3] - 48);
+			answer_date.day = 10 * (curr_date[6] - 48) + (curr_date[7] - 48);
+			answer_date.month = 10 * (curr_date[4] - 48) + (curr_date[5] - 48);
+			cout << "Восход луны:" << answer_date << endl;
 		}
-		else if (last_row.El > 0 and my_row.El <= 0) {
-			cout << "Заход луны:" << my_row.data << endl;
+		else if (last_El > 0 and El <= 0) {
+			answer_date.hour = (HMS[0] - 48) * 10 + (HMS[1] - 48);
+			answer_date.minute = (HMS[2] - 48) * 10 + (HMS[3] - 48);
+			answer_date.second = (HMS[4] - 48) * 10 + (HMS[5] - 48);
+			answer_date.year = 1000 * (curr_date[0] - 48) + 100 * (curr_date[1] - 48) + 10 * (curr_date[2] - 48) + (curr_date[3] - 48);
+			answer_date.day = 10 * (curr_date[6] - 48) + (curr_date[7] - 48);
+			answer_date.month = 10 * (curr_date[4] - 48) + (curr_date[5] - 48);
+			cout << "Заход луны:" << answer_date << endl;
 		}
-		if (max_El < my_row.El) {
-			date = my_row.data;
-			max_El = my_row.El;
+		if (max_El < El) {
+			date.hour = (HMS[0] - 48) * 10 + (HMS[1] - 48);
+			date.minute = (HMS[2] - 48) * 10 + (HMS[3] - 48);
+			date.second = (HMS[4] - 48) * 10 + (HMS[5] - 48);
+			date.year = 1000 * (curr_date[0] - 48) + 100 * (curr_date[1] - 48) + 10 * (curr_date[2] - 48) + (curr_date[3] - 48);
+			date.day = 10 * (curr_date[6] - 48) + (curr_date[7] - 48);
+			date.month = 10 * (curr_date[4] - 48) + (curr_date[5] - 48);
+			max_El = El;
 		}
-		last_row = my_row;
-
-		if (is_found_date and ((10 * (curr_date[6] - 48) + (curr_date[7] - 48)) != full_date.day or
-			(10 * (curr_date[4] - 48) + (curr_date[5] - 48)) != full_date.month)) {
+		last_El = El;
+		if (curr_date[0] == '\0' or (is_found_date and ((10 * (curr_date[6] - 48) + (curr_date[7] - 48)) != full_date.day or
+			(10 * (curr_date[4] - 48) + (curr_date[5] - 48)) != full_date.month))) {
 			break;
 		}
 	}
