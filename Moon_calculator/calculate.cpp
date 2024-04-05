022f58f1a3ca9aeb2d35febe1693be3e156b9aba
JartiX <artempilavin@gmail.com>
2024-04-05 13:07:36 +0800

    Update calculate.cpp

--- Moon_calculator/calculate.cpp
+++ Moon_calculator/calculate.cpp
@@ -11,7 +11,8 @@ void calculate(string& path, Datetime& full_date) {
 		cerr << "Ошибка при чтении файла";
 	}
 
-	char skip[14];
+	char line[100];
+
 	char curr_date[9];
 	char HMS[7];
 
@@ -25,20 +26,27 @@ void calculate(string& path, Datetime& full_date) {
 	answer_date = Datetime();
 	Datetime date;
 
-	file >> skip >> skip >> skip >> skip >> skip >> skip >> skip >> skip;
+	file.getline(line, 100);
+	file.getline(line, 100);
 
 	cout << "Дата: " << full_date << endl;
 
 	while (true) {
-		file >> curr_date >> HMS >> skip >> skip >> last_El >> skip >> skip >> skip;
+		file.getline(line, 100);
+		istringstream iss(line);
+		iss >> curr_date;
 		if ((10 * (curr_date[4] - 48) + (curr_date[5] - 48)) != full_date.month or
 			(10 * (curr_date[6] - 48) + (curr_date[7] - 48)) != full_date.day) {
 			continue;
 		}
+		iss >> HMS >> last_El >> last_El >> last_El;
 		break;
 	}
 	while (true) {
-		file >> curr_date >> HMS >> skip >> skip >> El >> skip >> skip >> skip;
+		file.getline(line, 100);
+		istringstream isss(line);
+		isss >> curr_date >> HMS >> El >> El >> El;
+
 		if (last_El < 0 and El >= 0) {
 			make_date(answer_date, HMS, curr_date);
 			cout << "Восход луны:" << answer_date << endl;
